cfecefd28fb664828d0c3b5062803fc1ad6b07a3
Abby Lortz <abilenelortz03@gmail.com>
2018-06-26 14:59:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ $(document).ready(function (){
 
 
 $.ajax({
-  api_key:"2Wmv9FWaICCS6j3Bm0KwdzFh6jiksrhg"
-  url:"https://api.github.com/search/code?q=addClass+user:mozilla"
+  api_key:"2Wmv9FWaICCS6j3Bm0KwdzFh6jiksrhg",
+  url:"https://api.github.com/search/code?q=addClass+user:mozilla",
+  
 })
 
